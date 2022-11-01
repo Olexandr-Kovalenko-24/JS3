14f56187677e317c8fe3fe843a7699f3c4ec077d
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-01 19:18:33 +0200

    add atribut defer

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
+const h1 = document.querySelector('h1');
+
+const id = setInterval(tick, 1000);
+let counter = 0;
+
+function tick() {
+    h1.textContent = ++counter;
+}
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    clearInterval(id);
+})
+
 // const h1 = document.querySelector('h1');
 
 // const id = setInterval(tick, 1000);
 // let counter = 0;
 
-// function tick() {
+// function tick(){
 //     h1.textContent = ++counter;
 // }
 
 // const btn = document.querySelector('button');
-
-// btn.addEventListener('click', function(){
+// btn.addEventListener('click', function() {
 //     clearInterval(id);
 // })
-
-const h1 = document.querySelector('h1');
-
-const id = setTimeout(tick, 1000);
-let counter = 0;
-
-function tick() {
-    h1.textContent = ++counter;
-    id = setTimeout(tick, 1000)
-}
-
-const btn = document.querySelector('button');
-btn.addEventListener('click', function(){
-    clearTimeout(id);
-})
