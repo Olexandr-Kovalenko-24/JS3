e092a03b8e840b39f0b01dce9180221fab913de1
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-01 15:28:46 +0200

    timeEnd

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-setTimeout(function (number=10){
-    let sum = 0;
-for (let i = 0; i <= number; i++) {
-    sum += i;  
+// setTimeout(function (number=10){
+//     let sum = 0;
+// for (let i = 0; i <= number; i++) {
+//     sum += i;  
+// }
+// console.log(sum);
+// }, 5000)
+
+function sum (){
+    let summary = 0;
+    for (let i = 0; i < 10000; i++) {
+        summary += i;  
+    }
+    console.log(summary);
+    console.timeEnd(1);
 }
-console.log(sum);
-}, 5000)
+
+console.time(1);
+setTimeout(sum, 3000);
