44e6617a38057b0009b9e3a9079aab6c3674efe8
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-01 19:25:06 +0200

    JSON

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-const id = setInterval(tick, 1000);
-let counter = 0;
+// const id = setInterval(tick, 1000);
+// let counter = 0;
 
-function tick() {
-    h1.textContent = ++counter;
-}
+// function tick() {
+//     h1.textContent = ++counter;
+// }
 
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
 
-btn.addEventListener('click', function(){
-    clearInterval(id);
-})
+// btn.addEventListener('click', function(){
+//     clearInterval(id);
+// })
 
 // const h1 = document.querySelector('h1');
 
@@ -26,3 +26,29 @@ btn.addEventListener('click', function(){
 // btn.addEventListener('click', function() {
 //     clearInterval(id);
 // })
+
+
+const user = {
+    firstName: 'Alex',
+    lastName: 'Doe',
+    age: 25,
+    telNum: '+380432234234',
+    footSize: undefined,
+    superId: Symbol(),
+    address: null,
+    sayHello: function(){
+        return `${this.firstName} say: Hello!`
+    }
+}
+
+const str = JSON.stringify(user);
+
+JSON.parse(str); 
+
+/* return
+address: null
+age:25
+firstName:"Alex"
+lastName:"Doe"
+telNum:"+380432234234"
+*/
