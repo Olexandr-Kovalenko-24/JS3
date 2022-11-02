c43496725d9b9f3406031aae42c1f2a0d0b6ec97
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-02 16:54:23 +0200

    task createCard after fetch request resolve

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const resultOfRequest = fetch('./data.json');
-console.log(resultOfRequest);
+// console.log(resultOfRequest);
 
 resultOfRequest
-.then((response)=>{
-    console.log('promise ok')
-    return response.json();
-}).then((successData)=>{
-    console.log(successData);
+.then((response)=>response.json())
+.then((successData)=>{
+    const card = createUserCard(successData);
+    const root = document.querySelector('#root');
+    root.append(card);
 })
 .catch((reject)=>{
     console.log(reject);
 })
+
+
+function createUserCard(user){
+    const h2 = createElement('h2', {classNames: ['username']}, user.firstName, user.lastName);
+    const p = createElement('p', {}, user.telNum);
+    const div = createElement('div', {classNames: ['card-wrapper']}, h2,p);
+    return div;
+}
+
+
+function createElement(type, {classNames=[]}, ...children){
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...children);
+    return elem;
+}
