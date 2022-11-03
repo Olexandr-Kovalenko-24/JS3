31863c77fe38f03ed491f357c06afc73186409f2
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-03 14:29:39 +0200

    Create new Promise

--- index.js
+++ index.js
@@ -1,29 +1,13 @@
-const resultOfRequest = fetch('./data.json');
-// console.log(resultOfRequest);
-
-resultOfRequest
-.then((response)=>response.json())
-.then((successData)=>{
-    const card = createUserCard(successData);
-    const root = document.querySelector('#root');
-    root.append(card);
-})
-.catch((reject)=>{
-    console.log(reject);
+const promise = new Promise(function(resolve, reject){
+    if(2+2 === 4){
+        resolve('It`s 4!');
+    } else{
+        reject('It`s not 4! How?!!!');
+    }
 })
 
-
-function createUserCard(user){
-    const h2 = createElement('h2', {classNames: ['username']}, `${user.firstName} ${user.lastName}`);
-    const p = createElement('p', {}, user.telNum);
-    const div = createElement('div', {classNames: ['card-wrapper']}, h2,p);
-    return div;
-}
-
-
-function createElement(type, {classNames=[]}, ...children){
-    const elem = document.createElement(type);
-    elem.classList.add(...classNames);
-    elem.append(...children);
-    return elem;
-}
+promise.then((res)=>{
+    console.log('Resolve: ', res);
+}, (error)=>{
+    console.log('Reject: ', error);
+});
