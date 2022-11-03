0213c72e691b7bf3f4c988e0e8b760c94a5606c8
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-03 17:05:03 +0200

    Add photo of products

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ resultOfRequest
         const root = document.querySelector('#root');
         const productCardArray = products.map(createProductCard);
         root.append(...productCardArray);
-        // root.addEventListener('load', pageLoadHandler);
     })
     .catch((rej) => {
         console.log(rej);
@@ -19,7 +18,9 @@ resultOfRequest
 function createProductCard(product) {
     const h2 = createElement('h2', { classNames: ['name'] }, product.title);
     const p = createElement('p', {}, product.description);
-    const div = createElement('div', { classNames: ['card-wrapper'] }, h2, p);
+    const image = createImageWrapper(product);
+    const price = createElement('p', {}, `${product.price} UAH`);
+    const div = createElement('div', { classNames: ['card-wrapper'] }, h2, p, price, image);
     return div;
 }
 
@@ -31,7 +32,28 @@ function createElement(type, { classNames = [] }, ...children) {
     return elem;
 }
 
+function createImageWrapper(product) {
+    const imageWrapper = createElement('div', { classNames: ['image-wrapper'] }, product.title[0])
+    imageWrapper.setAttribute('id', `wrapper-${product.id}`);
+    const avatar = createImage(product);
+    return imageWrapper;
+}
 
-// function pageLoadHandler(event){
-// event.classList.add('loader')
-// }
+function createImage(product) {
+    const avatar = document.createElement('img');
+    avatar.setAttribute('src', product.images[1]);
+    avatar.dataset.id = product.id;
+    avatar.addEventListener('load', imageLoadHandler);
+    avatar.addEventListener('error', imageErrorHandler);
+    avatar.classList.add('photo');
+    return avatar;
+}
+
+function imageLoadHandler({ target }) {
+    const parentWrapper = document.querySelector((`#wrapper-${target.dataset.id}`));
+    parentWrapper.append(target);
+}
+
+function imageErrorHandler(event) {
+    event.target.remove();
+}
