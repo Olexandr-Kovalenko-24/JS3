f277018e393175f4ee91f0e54d72030dfb1604cc
Olexandr Kovalenko <22bendera.s@gmail.com>
2022-11-03 16:20:47 +0200

    Task with load 30 products and rotate spinner res

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const promise = new Promise(function(resolve, reject){
-    if(2+2 === 4){
-        resolve('It`s 4!');
-    } else{
-        reject('It`s not 4! How?!!!');
-    }
-})
+const resultOfRequest = fetch('https://dummyjson.com/products');
 
-promise.then((res)=>{
-    console.log('Resolve: ', res);
-}, (error)=>{
-    console.log('Reject: ', error);
-});
+resultOfRequest
+    .then((res) => res.json())
+    .then(({ products }) => {
+        const root = document.querySelector('#root');
+        const productCardArray = products.map(createProductCard);
+        root.append(...productCardArray);
+        // root.addEventListener('load', pageLoadHandler);
+    })
+    .catch((rej) => {
+        console.log(rej);
+    })
+    .finally(() => {
+        const loader = document.querySelector('.loader');
+        loader.classList.remove('loader');
+    })
+
+function createProductCard(product) {
+    const h2 = createElement('h2', { classNames: ['name'] }, product.title);
+    const p = createElement('p', {}, product.description);
+    const div = createElement('div', { classNames: ['card-wrapper'] }, h2, p);
+    return div;
+}
+
+
+function createElement(type, { classNames = [] }, ...children) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...children);
+    return elem;
+}
+
+
+// function pageLoadHandler(event){
+// event.classList.add('loader')
+// }
